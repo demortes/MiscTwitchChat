e04c64474877450fd2aa59a516180ff42da1029a
Kevin 'Demortes' Dethlefs <webmaster@demortes.com>
2019-01-01 00:44:32 -0600

    Fixing items through Ai and whatnot. Figured out I forgot to add the xml comments on release.

--- MiscTwitchChat/App_Start/FilterConfig.cs
+++ MiscTwitchChat/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace MiscTwitchChat
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
