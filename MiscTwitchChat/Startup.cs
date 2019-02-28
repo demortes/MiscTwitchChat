2a929c8be5a6fba457417e780064600e542902d8
Kevin Dethlefs <kdethlefs@gmail.com>
2019-02-27 23:10:35 -0600

    Adding swagger.

--- MiscTwitchChat/Startup.cs
+++ MiscTwitchChat/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MiscTwitchChat
 {
@@ -30,6 +31,11 @@ namespace MiscTwitchChat
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IConfiguration>(Configuration);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Demortes' Random Chatbot API's", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +50,16 @@ namespace MiscTwitchChat
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demortes' Random Chatbot API's");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
