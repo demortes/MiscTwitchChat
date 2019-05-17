4b74412f3c0bbc4aa80786d3594464b0c7b31354
Kevin Dethlefs <kdethlefs@gmail.com>
2019-05-16 20:33:01 -0500

    Cards API hit.

---
+++ MiscTwitchChat/Controllers/CardsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiscTwitchChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+ 
+        // GET: api/Cards
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            var blackCards = System.IO.File.ReadAllLines("black_cards.txt");
+            var whiteCards = System.IO.File.ReadAllLines("white_cards.txt");
+
+            var blackCard = blackCards[new Random().Next(0, blackCards.Length - 1)];
+            var whiteCard = whiteCards[new Random().Next(0, whiteCards.Length - 1)];
+
+            var returnString = $"Prompt: {blackCard}\r\nReponse: {whiteCard}";
+            return returnString;
+        }
+
+    }
+}
