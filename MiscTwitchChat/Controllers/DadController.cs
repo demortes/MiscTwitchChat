99bbe88d92e2dd29d67603882664db486aefbeb3
Kevin Dethlefs <kdethlefs@gmail.com>
2019-08-17 18:51:13 -0500

    Dad joke controller.

---
+++ MiscTwitchChat/Controllers/DadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiscTwitchChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DadController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<String> GetDadJoke()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+            var resp = await client.GetAsync("https://icanhazdadjoke.com");
+            var joke = await resp.Content.ReadAsStringAsync();
+            return joke;
+        }
+    }
+}
