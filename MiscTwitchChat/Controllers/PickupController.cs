6b7039aa93493daa353236de637eb010dcab2b5c
Kevin Dethlefs <kdethlefs@gmail.com>
2019-12-23 21:45:43 -0600

    Adding pickup line generator/api consumer.

---
+++ MiscTwitchChat/Controllers/PickupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiscTwitchChat.Models;
+using Newtonsoft.Json;
+
+namespace MiscTwitchChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PickupController : ControllerBase
+    {
+        private string endpoint = "http://pebble-pickup.herokuapp.com/tweets/random";
+        [HttpGet]
+        public async System.Threading.Tasks.Task<string> GetAsync()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+            var response = await client.GetAsync(endpoint);
+            var contentReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+            JsonSerializer serializer = new JsonSerializer();
+            var dogResponse = (PickupResponse)serializer.Deserialize(contentReader, typeof(PickupResponse));
+            return dogResponse.tweet;
+        }
+    }
+}
---
+++ MiscTwitchChat/Models/PickupResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiscTwitchChat.Models
+{
+
+    public class PickupResponse
+    {
+        public string _id { get; set; }
+        public string username { get; set; }
+        public string tweet { get; set; }
+        public int __v { get; set; }
+    }
+
+}
