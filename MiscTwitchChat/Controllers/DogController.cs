5cb2b5cd0a16882d3fbc83c1830a8bed06d0f552
Kevin Dethlefs <kdethlefs@gmail.com>
2019-12-23 21:40:20 -0600

    Adding dog API

---
+++ MiscTwitchChat/Controllers/DogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiscTwitchChat.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace MiscTwitchChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : ControllerBase
+    {
+        private string endpoint = "https://dog-api.kinduff.com/api/facts";
+
+        [HttpGet]
+        public async System.Threading.Tasks.Task<string> GetAsync()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+            var response = await client.GetAsync(endpoint);
+            var contentReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+            JsonSerializer serializer = new JsonSerializer();
+            var dogResponse = (DogResponse)serializer.Deserialize(contentReader, typeof(DogResponse));
+            return dogResponse.facts[0];
+        }
+    }
+}
---
+++ MiscTwitchChat/Models/DogResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiscTwitchChat.Models
+{
+    public class DogResponse
+    {
+        public string[] facts { get; set; }
+        public bool success { get; set; }
+    }
+
+}
