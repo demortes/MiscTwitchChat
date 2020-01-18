fff2b4aefa3f746e306f6f7da90b0c900c3d9e14
Kevin Dethlefs <kdethlefs@gmail.com>
2020-01-18 14:59:45 -0600

    Adding insult API

---
+++ MiscTwitchChat/Controllers/InsultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiscTwitchChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InsultController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> InsultAsync(string channel, string user, string target = null)
+        {
+            var url = "https://insult.mattbas.org/api/insult.txt";
+            var client = new HttpClient();
+            if(!string.IsNullOrWhiteSpace(target))
+            {
+                url += $"?who={target}";
+            }
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
