70e4b9d6898ede587e7dc38e1f1978b3a861f1b9
Kevin Dethlefs <webmaster@demortes.com>
2020-03-04 23:18:42 -0600

    Starting blame module.

---
+++ DiscordBot/Modules/BlameModule.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class BlameModule : ModuleBase<SocketCommandContext>
+    {
+        private IConfiguration _config;
+        private IDiscordClient _client;
+
+        public BlameModule(IConfiguration config, IDiscordClient discordClient)
+        {
+            _config = config;
+            _client = discordClient;
+        }
+
+        [Command("blame")]
+        public async Task Insult(IUser target = null)
+        {
+            if (target == null)
+            {
+                await ReplyAsync("Welp, you can't blame anyone but yourself apparently.");
+                return;
+            }
+
+            await ReplyAsync(reply);
+        }
+    }
+}
