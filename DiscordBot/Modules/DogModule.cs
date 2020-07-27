2ed26be8e3586936682fb067b330b2f3b2dc8380
Kevin Dethlefs <webmaster@demortes.com>
2020-07-27 17:58:53 -0500

    Adding new modules.

---
+++ DiscordBot/Modules/DogModule.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class DogModule : ModuleBase<SocketCommandContext>
+    {
+        private IConfiguration _config;
+
+        public DogModule(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Command("dog")]
+        public async Task Dog(IUser target = null)
+        {
+            var channel = Context.Channel.Name;
+            var user = Context.User.Username;
+            var url = _config.GetValue<string>("BaseAPIUrl");
+            var apiService = new DemAPI.Client(url, new HttpClient());
+            apiService.ReadResponseAsString = true;
+            var reply = await apiService.ApiDogAsync();
+            await ReplyAsync(reply);
+        }
+    }
+}
