97fc7fcd5ab738f3660f40645c97eb8c05f3b1cb
Kevin Dethlefs <kdethlefs@gmail.com>
2021-11-24 17:23:54 -0600

    API for dog facts gone, refactoring for txt file.

--- MiscTwitchChat/Controllers/DogController.cs
+++ MiscTwitchChat/Controllers/DogController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MiscTwitchChat.Models;
 using Newtonsoft.Json;
-using System.IO;
+using System;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 
 namespace MiscTwitchChat.Controllers
 {
@@ -12,19 +10,13 @@ namespace MiscTwitchChat.Controllers
     [ApiController]
     public class DogController : ControllerBase
     {
-        private readonly string endpoint = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1";
-
         [HttpGet]
         public async System.Threading.Tasks.Task<string> GetAsync()
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("text/plain"));
-            var response = await client.GetAsync(endpoint);
-            var contentReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+            var filePtr = System.IO.File.ReadAllLines("dog_facts.txt");
+            var fact = filePtr[new Random().Next(filePtr.Length - 1)];
             JsonSerializer serializer = new JsonSerializer();
-            var dogResponse = (DogResponse[])serializer.Deserialize(contentReader, typeof(DogResponse[]));
-            return dogResponse.First().Fact;
+            return fact;
         }
     }
 }
