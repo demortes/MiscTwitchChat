03c14c5586836a53bce6cf15429237cf8a28e5be
Demortes <kdethlefs@gmail.com>
2025-03-14 02:23:59 -0500

    add name generation functionality (#158)
    - Updated `launchSettings.json` to remove the `commandName` property from the `MiscTwitchChat` profile.
    - Introduced `NameModule` in `NameModule.cs` with a slash command `GenerateName` for generating names based on game and item inputs.
    - Added `NameController` in `NameController.cs` to handle HTTP GET requests for name generation, utilizing environment variables for API key and base URL, and implementing error handling for API responses.

---
+++ DiscordBot/Modules/NameModule.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class NameModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        private IConfiguration _config;
+
+        public NameModule(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [SlashCommand("generate-name", "Generate a name to be used in a video game. Pick the game, and the object you want a name for.")]
+        public async Task GenerateName(string game, string item)
+        {
+            // To be finished once API is published.
+            //var channel = Context.Channel.Name;
+            //var user = Context.User.Username;
+            //var url = _config.GetValue<string>("BaseAPIUrl");
+            //var apiService = new DemAPI.Client(url, new HttpClient())
+            //{
+            //    ReadResponseAsString = true
+            //};
+            //var reply = await apiService.ApiCardsDeleteAsync(channel, user, target?.Username);
+            //await RespondAsync(reply);
+        }
+    }
+}
